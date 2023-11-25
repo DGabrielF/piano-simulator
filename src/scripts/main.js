c3ffeee31d22463949445ab830cb5a5bf8a6ccf8
gabriel <dgabrielfof@gmail.com>
2023-11-25 00:55:54 -0300

    add:adjustments for smaller screens

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,16 @@
-const pianoKeys = document.querySelectorAll(".piano-keyboard .key")
-const volumeSlider = document.querySelector(".volume-slider input")
+const pianoKeys = document.querySelectorAll(".piano-keyboard .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const hideKeys = document.querySelector(".keys-check input");
 
 const mappedKeys = [];
-let audio = new Audio("src/tunes/a.wav")
+let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
   audio.src = `src/tunes/${key}.wav`;
   audio.play();
   const clickedKey = document.querySelector(`[data-key="${key}"]`);
   clickedKey.classList.add("active");
-  setTimeout(()=>{clickedKey.classList.remove("active")},200)
+  setTimeout(()=>{clickedKey.classList.remove("active")},200);
 }
 
 pianoKeys.forEach((key) => {
@@ -21,7 +22,19 @@ document.addEventListener("keydown", e => {
   if (mappedKeys.includes(e.key))
   {playTune(e.key)}
 });
-const handleVolume = (e) => {
-  audio.volume = e.target.value
+
+volumeSlider.addEventListener("input", (e) => audio.volume = e.target.value);
+hideKeys.addEventListener("click", () => {
+  pianoKeys.forEach(key => key.classList.toggle("hide"))
+})
+
+function adjustLayout() {
+  const container = document.querySelector('.container');
+  if (window.innerWidth <= 640) {
+    container.classList.add("rotate")
+  } else {
+    container.classList.remove("rotate")
+  }
 }
-volumeSlider.addEventListener("input", handleVolume)
+window.addEventListener('resize', adjustLayout);
+adjustLayout();
